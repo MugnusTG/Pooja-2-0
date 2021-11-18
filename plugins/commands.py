a20eae269b44a2c53f4948b3d221153163ae5f30
MagnusTG <93865402+ateinstein12@users.noreply.github.com>
2021-11-18 14:15:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,7 +40,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')          
             ],[
             InlineKeyboardButton('🎁 Help', callback_data='help'),
-            InlineKeyboardButton('👿 About', callback_data='about')
+            InlineKeyboardButton('👑 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
